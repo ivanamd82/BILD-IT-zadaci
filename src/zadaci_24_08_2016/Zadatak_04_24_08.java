0f36a17864a2a0f58a45fa625678c6c89a61b754
Ivana <Ivana@Cupici>
2016-08-30 10:00:22 +0200

    fix

--- src/zadaci_24_08_2016/Zadatak_04_24_08.java
+++ src/zadaci_24_08_2016/Zadatak_04_24_08.java
@@ -1,7 +1,7 @@
 package zadaci_24_08_2016;
 /* 10.19
  * (Mersenne prime) A prime number is called a Mersenne prime if it can be written 
- * in the form 2^p - 1 for some positive integer p. Write a program that finds
+ * in the form 2p - 1 for some positive integer p. Write a program that finds
  * all Mersenne primes with p … 100 and displays the output as shown below.
  * (Hint: You have to use BigInteger to store the number, because it is too big to
  * be stored in long. Your program may take several hours to run.)
@@ -21,7 +21,7 @@ public class Zadatak_04_24_08 {
 		//petlja od 2 do 100
 		for(int i = 2; i <= 100; i++){
 			if(isPrime(i)) {												//ako je broj p primaran 
-				if(Zadatak_03_24_08.isPrimeBig(getMersenne(i)))						//provjeriti da li je i Mersenne primaran
+				if(isPrime(getMersenne(i).intValue()))						//provjeriti da li je i Mersenne primaran
 					System.out.printf("%-10d%-10s\n", i, getMersenne(i));	//ako jeste ispisati ga							
 			}
 		}
@@ -36,7 +36,7 @@ public class Zadatak_04_24_08 {
 		}
 		return true;
 	}
-	//dobijanje Mersenne broja - broj koji se moze zapisati u obliku 2^p - 1
+	//dobijanje Mersenne broja - broj koji se moze zapisati u obliku 2p - 1
     public static BigInteger getMersenne(int p) {
         return new BigInteger("2").pow(p).subtract(BigInteger.ONE);
     }
